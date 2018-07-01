f99379b49a8ea618518a1a27da2551bd9f2a7b74
Reverend Jesse Vanover <ReverendV92@users.noreply.github.com>
2018-07-01 14:57:37 -0700

    Sync

---
+++ AddOns/AHL2_Players/lua/autorun/sh_v92_ahl2_players.lua
@@ -0,0 +1,155 @@
+
+AddCSLuaFile()
+
+function AddPlayerModel( name, model ) player_manager.AddValidModel( name, model ) end
+function AddPlayerHands( name, model, skin, bodygroup ) player_manager.AddValidHands( name, model, skin, bodygroup ) end
+
+-------------------------------------------------------
+-------------------------------------------------------
+--	ACTION HALF-LIFE 2
+-------------------------------------------------------
+-------------------------------------------------------
+--	Players
+-------------------------------------------------------
+-------------------------------------------------------
+
+AddPlayerModel("AHL2 Detective", "models/jessev92/ahl2/characters/detective.mdl" )
+AddPlayerHands( "AHL2 Detective", "models/weapons/c_arms_refugee.mdl", 0, "01000000" )
+list.Set( "PlayerOptionsAnimations", "AHL2 Detective", { "menu_combine" } )
+util.PrecacheModel(	"models/jessev92/ahl2/characters/detective.mdl"	)
+util.PrecacheModel(	"models/weapons/c_arms_refugee.mdl"	)
+
+AddPlayerModel("AHL2 Dick", "models/jessev92/ahl2/characters/dick.mdl" )
+AddPlayerHands( "AHL2 Dick", "models/weapons/c_arms_refugee.mdl", 0, "01000000" )
+list.Set( "PlayerOptionsAnimations", "AHL2 Dick", { "menu_combine" } )
+util.PrecacheModel(	"models/jessev92/ahl2/characters/dick.mdl"	)
+util.PrecacheModel(	"models/weapons/c_arms_refugee.mdl"	)
+
+--
+AddPlayerModel("AHL2 El Jefe", "models/jessev92/ahl2/characters/eljefe.mdl" )
+AddPlayerHands( "AHL2 El Jefe", "models/weapons/c_arms_refugee.mdl", 0, "01000000" )
+list.Set( "PlayerOptionsAnimations", "AHL2 El Jefe", { "menu_combine" } )
+util.PrecacheModel(	"models/jessev92/ahl2/characters/eljefe.mdl"	)
+util.PrecacheModel(	"models/weapons/c_arms_refugee.mdl"	)
+--]]
+
+AddPlayerModel("AHL2 Frank Castle", "models/jessev92/ahl2/characters/frank.mdl" )
+AddPlayerHands( "AHL2 Frank Castle", "models/weapons/c_arms_refugee.mdl", 0, "01000000" )
+list.Set( "PlayerOptionsAnimations", "AHL2 Frank", { "menu_combine" } )
+util.PrecacheModel(	"models/jessev92/ahl2/characters/frank.mdl"	)
+util.PrecacheModel(	"models/weapons/c_arms_refugee.mdl"	)
+
+AddPlayerModel("AHL2 Hammer Time", "models/jessev92/ahl2/characters/HammerTime.mdl" )
+AddPlayerHands( "AHL2 Hammer Time", "models/weapons/c_arms_refugee.mdl", 0, "01000000" )
+list.Set( "PlayerOptionsAnimations", "AHL2 Hammer Time", { "menu_combine" } )
+util.PrecacheModel(	"models/jessev92/ahl2/characters/HammerTime.mdl"	)
+util.PrecacheModel(	"models/weapons/c_arms_refugee.mdl"	)
+
+AddPlayerModel("AHL2 Hitman", "models/jessev92/ahl2/characters/Hitman.mdl" )
+AddPlayerHands( "AHL2 Hitman", "models/weapons/c_arms_refugee.mdl", 0, "01000000" )
+list.Set( "PlayerOptionsAnimations", "AHL2 Hitman", { "menu_combine" } )
+util.PrecacheModel(	"models/jessev92/ahl2/characters/Hitman.mdl"	)
+util.PrecacheModel(	"models/weapons/c_arms_refugee.mdl"	)
+
+--
+AddPlayerModel("AHL2 Johnny Cage", "models/jessev92/ahl2/characters/jc.mdl" )
+AddPlayerHands( "AHL2 Johnny Cage", "models/weapons/c_arms_refugee.mdl", 0, "01000000" )
+list.Set( "PlayerOptionsAnimations", "AHL2 Johnny Cage", { "menu_combine" } )
+util.PrecacheModel(	"models/jessev92/ahl2/characters/jc.mdl"	)
+util.PrecacheModel(	"models/weapons/c_arms_refugee.mdl"	)
+
+AddPlayerModel("AHL2 Jerkov", "models/jessev92/ahl2/characters/Jerkov.mdl" )
+AddPlayerHands( "AHL2 Jerkov", "models/weapons/c_arms_refugee.mdl", 0, "01000000" )
+list.Set( "PlayerOptionsAnimations", "AHL2 Jerkov", { "menu_combine" } )
+util.PrecacheModel(	"models/jessev92/ahl2/characters/Jerkov.mdl"	)
+util.PrecacheModel(	"models/weapons/c_arms_refugee.mdl"	)
+--]]
+
+AddPlayerModel("AHL2 Jersey", "models/jessev92/ahl2/characters/Jersey.mdl" )
+AddPlayerHands( "AHL2 Jersey", "models/weapons/c_arms_refugee.mdl", 0, "01000000" )
+list.Set( "PlayerOptionsAnimations", "AHL2 Jersey", { "menu_combine" } )
+util.PrecacheModel(	"models/jessev92/ahl2/characters/Jersey.mdl"	)
+util.PrecacheModel(	"models/weapons/c_arms_refugee.mdl"	)
+
+AddPlayerModel("AHL2 Johnson", "models/jessev92/ahl2/characters/Johnson.mdl" )
+AddPlayerHands( "AHL2 Johnson", "models/weapons/c_arms_refugee.mdl", 0, "01000000" )
+list.Set( "PlayerOptionsAnimations", "AHL2 Johnson", { "menu_combine" } )
+util.PrecacheModel(	"models/jessev92/ahl2/characters/Johnson.mdl"	)
+util.PrecacheModel(	"models/weapons/c_arms_refugee.mdl"	)
+
+--
+AddPlayerModel("AHL2 Minky", "models/jessev92/ahl2/characters/Minky.mdl" )
+AddPlayerHands( "AHL2 Minky", "models/weapons/c_arms_refugee.mdl", 0, "01000000" )
+list.Set( "PlayerOptionsAnimations", "AHL2 Minky", { "menu_combine" } )
+util.PrecacheModel(	"models/jessev92/ahl2/characters/Minky.mdl"	)
+util.PrecacheModel(	"models/weapons/c_arms_refugee.mdl"	)
+
+AddPlayerModel("AHL2 Power Trip", "models/jessev92/ahl2/characters/PowerTrip.mdl" )
+AddPlayerHands( "AHL2 Power Trip", "models/weapons/c_arms_refugee.mdl", 0, "01000000" )
+list.Set( "PlayerOptionsAnimations", "AHL2 Power Trip", { "menu_combine" } )
+util.PrecacheModel(	"models/jessev92/ahl2/characters/PowerTrip.mdl"	)
+util.PrecacheModel(	"models/weapons/c_arms_refugee.mdl"	)
+--]]
+
+AddPlayerModel("AHL2 Wingtsun", "models/jessev92/ahl2/characters/Wingtsun.mdl" )
+AddPlayerHands( "AHL2 Wingtsun", "models/weapons/c_arms_refugee.mdl", 0, "01000000" )
+list.Set( "PlayerOptionsAnimations", "AHL2 Wingtsun", { "menu_combine" } )
+util.PrecacheModel(	"models/jessev92/ahl2/characters/Wingtsun.mdl"	)
+util.PrecacheModel(	"models/weapons/c_arms_refugee.mdl"	)
+
+-------------------------------------------------------
+-------------------------------------------------------
+--	NPCs
+-------------------------------------------------------
+-------------------------------------------------------
+local Category = "NPCG: Action Half-Life 2"
+-------------------------------------------------------
+-------------------------------------------------------
+--[[
+local NPCG_AHL2_Detective		= {	Name = "Detective",				Class = "npcg_ahl2_detective",			Category = Category	}		list.Set( "NPC", "npcg_ahl2_detective", 			NPCG_AHL2_Detective )
+local NPCG_AHL2_Detective_Spawn	= {	Name = "Detective Spawner",		Class = "npcg_ahl2_detective_spawn",	Category = Category	}		list.Set( "NPC", "npcg_ahl2_detective_spawn", 		NPCG_AHL2_Detective_Spawn )
+
+local NPCG_AHL2_Dick		= {	Name = "Dick",				Class = "npcg_ahl2_Dick",			Category = Category	}		list.Set( "NPC", "npcg_ahl2_Dick", 			NPCG_AHL2_Dick )
+local NPCG_AHL2_Dick_Spawn	= {	Name = "Dick Spawner",		Class = "npcg_ahl2_Dick_spawn",	Category = Category	}		list.Set( "NPC", "npcg_ahl2_Dick_spawn", 		NPCG_AHL2_Dick_Spawn )
+
+local NPCG_AHL2_ElJefe		= {	Name = "ElJefe",				Class = "npcg_ahl2_ElJefe",			Category = Category	}		list.Set( "NPC", "npcg_ahl2_ElJefe", 			NPCG_AHL2_ElJefe )
+local NPCG_AHL2_ElJefe_Spawn	= {	Name = "ElJefe Spawner",		Class = "npcg_ahl2_ElJefe_spawn",	Category = Category	}		list.Set( "NPC", "npcg_ahl2_ElJefe_spawn", 		NPCG_AHL2_ElJefe_Spawn )
+
+local NPCG_AHL2_Frank		= {	Name = "Frank",				Class = "npcg_ahl2_Frank",			Category = Category	}		list.Set( "NPC", "npcg_ahl2_Frank", 			NPCG_AHL2_Frank )
+local NPCG_AHL2_Frank_Spawn	= {	Name = "Frank Spawner",		Class = "npcg_ahl2_Frank_spawn",	Category = Category	}		list.Set( "NPC", "npcg_ahl2_Frank_spawn", 		NPCG_AHL2_Frank_Spawn )
+
+local NPCG_AHL2_HammerTime		= {	Name = "HammerTime",				Class = "npcg_ahl2_HammerTime",			Category = Category	}		list.Set( "NPC", "npcg_ahl2_HammerTime", 			NPCG_AHL2_HammerTime )
+local NPCG_AHL2_HammerTime_Spawn	= {	Name = "HammerTime Spawner",		Class = "npcg_ahl2_HammerTime_spawn",	Category = Category	}		list.Set( "NPC", "npcg_ahl2_HammerTime_spawn", 		NPCG_AHL2_HammerTime_Spawn )
+
+local NPCG_AHL2_Hitman		= {	Name = "Hitman",				Class = "npcg_ahl2_Hitman",			Category = Category	}		list.Set( "NPC", "npcg_ahl2_Hitman", 			NPCG_AHL2_Hitman )
+local NPCG_AHL2_Hitman_Spawn	= {	Name = "Hitman Spawner",		Class = "npcg_ahl2_Hitman_spawn",	Category = Category	}		list.Set( "NPC", "npcg_ahl2_Hitman_spawn", 		NPCG_AHL2_Hitman_Spawn )
+
+local NPCG_AHL2_JohnnyCage		= {	Name = "JohnnyCage",				Class = "npcg_ahl2_JohnnyCage",			Category = Category	}		list.Set( "NPC", "npcg_ahl2_JohnnyCage", 			NPCG_AHL2_JohnnyCage )
+local NPCG_AHL2_JohnnyCage_Spawn	= {	Name = "JohnnyCage Spawner",		Class = "npcg_ahl2_JohnnyCage_spawn",	Category = Category	}		list.Set( "NPC", "npcg_ahl2_JohnnyCage_spawn", 		NPCG_AHL2_JohnnyCage_Spawn )
+
+local NPCG_AHL2_Jerkov		= {	Name = "Jerkov",				Class = "npcg_ahl2_Jerkov",			Category = Category	}		list.Set( "NPC", "npcg_ahl2_Jerkov", 			NPCG_AHL2_Jerkov )
+local NPCG_AHL2_Jerkov_Spawn	= {	Name = "Jerkov Spawner",		Class = "npcg_ahl2_Jerkov_spawn",	Category = Category	}		list.Set( "NPC", "npcg_ahl2_Jerkov_spawn", 		NPCG_AHL2_Jerkov_Spawn )
+
+local NPCG_AHL2_Jersey		= {	Name = "Jersey",				Class = "npcg_ahl2_Jersey",			Category = Category	}		list.Set( "NPC", "npcg_ahl2_Jersey", 			NPCG_AHL2_Jersey )
+local NPCG_AHL2_Jersey_Spawn	= {	Name = "Jersey Spawner",		Class = "npcg_ahl2_Jersey_spawn",	Category = Category	}		list.Set( "NPC", "npcg_ahl2_Jersey_spawn", 		NPCG_AHL2_Jersey_Spawn )
+
+local NPCG_AHL2_Johnson		= {	Name = "Johnson",				Class = "npcg_ahl2_Johnson",			Category = Category	}		list.Set( "NPC", "npcg_ahl2_Johnson", 			NPCG_AHL2_Johnson )
+local NPCG_AHL2_Johnson_Spawn	= {	Name = "Johnson Spawner",		Class = "npcg_ahl2_Johnson_spawn",	Category = Category	}		list.Set( "NPC", "npcg_ahl2_Johnson_spawn", 		NPCG_AHL2_Johnson_Spawn )
+
+local NPCG_AHL2_Minky		= {	Name = "Minky",				Class = "npcg_ahl2_Minky",			Category = Category	}		list.Set( "NPC", "npcg_ahl2_Minky", 			NPCG_AHL2_Minky )
+local NPCG_AHL2_Minky_Spawn	= {	Name = "Minky Spawner",		Class = "npcg_ahl2_Minky_spawn",	Category = Category	}		list.Set( "NPC", "npcg_ahl2_Minky_spawn", 		NPCG_AHL2_Minky_Spawn )
+
+local NPCG_AHL2_PowerTrip		= {	Name = "PowerTrip",				Class = "npcg_ahl2_PowerTrip",			Category = Category	}		list.Set( "NPC", "npcg_ahl2_PowerTrip", 			NPCG_AHL2_PowerTrip )
+local NPCG_AHL2_PowerTrip_Spawn	= {	Name = "PowerTrip Spawner",		Class = "npcg_ahl2_PowerTrip_spawn",	Category = Category	}		list.Set( "NPC", "npcg_ahl2_PowerTrip_spawn", 		NPCG_AHL2_PowerTrip_Spawn )
+
+local NPCG_AHL2_Wingtsun		= {	Name = "Wingtsun",				Class = "npcg_ahl2_Wingtsun",			Category = Category	}		list.Set( "NPC", "npcg_ahl2_Wingtsun", 			NPCG_AHL2_Wingtsun )
+local NPCG_AHL2_Wingtsun_Spawn	= {	Name = "Wingtsun Spawner",		Class = "npcg_ahl2_Wingtsun_spawn",	Category = Category	}		list.Set( "NPC", "npcg_ahl2_Wingtsun_spawn", 		NPCG_AHL2_Wingtsun_Spawn )
+--]]
+if SERVER then
+
+	--	Add to client download
+	resource.AddWorkshop( "914598575" )	--	Action Half-Life 2 Players
+	resource.AddWorkshop( "851168781" )	--	V92 Base Character Materials
+	resource.AddWorkshop( "136077464" )	--	NPC Groups
+
+end
